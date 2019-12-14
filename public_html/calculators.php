85602ebafa48fb7fd6e319823296a702b1dd66e1
kevlampiev <kevlampiev@gmail.com>
2019-12-15 01:38:30 +0700

    added strange calcelators

---
+++ public_html/calculators.php
@@ -0,0 +1,20 @@
+<?php
+require("../engine/components/header.php");
+?>
+
+
+<main>
+
+    <?php
+    require("../engine/components/calc_comp.php");
+    ?>
+
+
+</main>
+<?php
+require("../engine/components/shout.php");
+?>
+
+<?php
+require("../engine/components/footer.php");
+?>
